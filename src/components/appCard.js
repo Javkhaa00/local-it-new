a93d92840f52e2e999d6977d4150354a2755e5c5
Pixel-De <57444983+Pixel-De@users.noreply.github.com>
2020-09-17 16:41:12 +0800

    first push

---
+++ src/components/appCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AppCard = () => {
+    
+    return (
+        <div className="appcard-container">
+            test
+        </div>
+    )
+}
+
+export default AppCard
---
+++ src/pages/theApp.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const TheApp = () => {
+    
+    return (
+        <div>
+            <h3>The App</h3>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TheApp
