79381b07546e46234e619bceb116f570860a91ce
Javkhaa00 <liplicker0423@gmail.com>
2020-09-17 17:37:56 +0800

    feat: header deleted css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import Landing from '../containers/landing';
-import Header from "../containers/header";
 import AppContainer from "../containers/theApp"
 
 export const Home = () => {
     return (
-<<<<<<< HEAD
-        <div className="app">
-            <Header />
-=======
         <div>
             <Landing />
->>>>>>> f788617a9e390271e4871df5c3c36132e23b246b
             <AppContainer />
         </div>
     );
