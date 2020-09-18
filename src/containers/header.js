d126b5923a53dba1bc2e2897c4270e2bb905257c
Javkhaa00 <liplicker0423@gmail.com>
2020-09-18 17:14:35 +0800

    feat: assets moved to public

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,67 +1,23 @@
-import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
+import React from 'react';
 
 const Header = () => {
 
-    const history = useHistory()
-
-    const goHome = (section) => {
-        history.push(`/?section=${section}`);
-        console.log(section)
-    }
-
-    const [classActive, setClass] = useState("hamburger");
-    const [menuSide, setMenu] = useState("header--nav");
-
     return (
         <div className="header">
-            <div className="header--logo">
-                <img
-                    onClick={() => {
-                        window.location = "/";
-                    }}
-                    src="/assets/local-it-logo.png" alt="logo"
-                />
+            <div className="header--wrapper">
+                <div className="header--wrapper--logo">
+                    <img src="/assets/local-it-logo.png" alt="header-logo"/>
+                </div>
+                <div className="header--wrapper--nav">
+                    <ul>
+                        <li><a>Start</a></li>
+                        <li><a>The App</a></li>
+                        <li><a>Who we are?</a></li>
+                        <li><a>Contact Us</a></li>
+                        <li><a>Are you a Local Business?</a></li>
+                    </ul>
+                </div>
             </div>
-            <div
-                className={classActive}
-                onClick={() => {
-                    if (classActive === "hamburger") {
-                        setClass("change");
-                        setMenu("header--nav-hidden");
-                    } else {
-                        setClass("hamburger");
-                        setMenu("header--nav");
-                    }
-                }}
-            >
-                <div className="stick" />
-                <div className="stick1" />
-                <div className="stick2" />
-            </div>
-            <nav className={menuSide}>
-                <ul>
-                    <li><div
-                        className="nav-buttons"
-                        onClick={() => goHome("start-section")} >
-                        Start</div></li>
-                    <li><div
-                        className="nav-buttons"
-                        onClick={() => goHome("theapp-section")} >
-                        The app</div></li>
-                    <li><div
-                        className="nav-buttons"
-                        onClick={() => goHome("whoweare-section")} >
-                        Who we are?</div></li>
-                    <li><div
-                        className="nav-buttons"
-                        onClick={() => goHome("contact-section")} >
-                        Contact Us</div></li>
-                    <li>
-                        <span className="header--button">Are you a Local Business?</span>
-                    </li>
-                </ul>
-            </nav>
         </div>
     )
 }
