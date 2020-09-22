35b9c53468a92f9c5de7d496a8f13a6675dc232c
Javkhaa00 <liplicker0423@gmail.com>
2020-09-22 15:03:14 +0800

    feat: contact section added

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -9,24 +9,33 @@ import {
 const Contact = () => {
     return (
         <div className="contact">
-            <div className="contact--text-wrapper">
-                <span className="contacts--text-wrapper--us">CONTACT US</span>
-                <span className="contacts--text-wrapper--title">Get In Touch</span>
-                <span className="contacts--text-wrapper--desc">
-                    Consectetur adipiscing elit, sed do eiusmod temport incididunt ut labore et dolor temprt incididunt ut labore dolore magna.
-                </span>
-                {/* <div className="contacts--text-wrappers">
-                    <FontAwesomeIcon icon={faPhoneAlt} size="lg" className="fontawesome" />
-                    <span className="contacts--texts">+1 878 878 7878</span>
+            <div className="contact--wrapper">
+                <div className="contact--wrapper--text-wrapper">
+                    <span className="contact--wrapper--text-wrapper--us">CONTACT US</span>
+                    <span className="contact--wrapper--text-wrapper--title">Get In Touch</span>
+                    <span className="contact--wrapper--text-wrapper--desc">
+                        Consectetur adipiscing elit, sed do eiusmod temport incididunt ut labore et dolor temprt incididunt ut labore dolore magna.
+                    </span>
+                    <div className="contact--wrapper--text-wrapper--icons">
+                        <FontAwesomeIcon icon={faPhoneAlt} size="lg" className="fontawesome" />
+                        <span className="contact--wrapper--text-wrapper--texts">+1 878 878 7878</span>
+                    </div>
+                    <div className="contact--wrapper--text-wrapper--icons" >
+                        <FontAwesomeIcon icon={faEnvelope} size="lg" className="fontawesome" />
+                        <span className="contact--wrapper--text-wrapper--texts">info@email.com</span>
+                    </div>
+                    <div className="contact--wrapper--text-wrapper--icons" >
+                        <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" className="fontawesome" style={{ paddingRight: "30px" }} />
+                        <span className="contact--wrapper--text-wrapper--texts">1750 13th St, Boulder, CO 80302</span>
+                    </div>
                 </div>
-                <div className="contacts--text-wrappers" style={{ paddingBottom: "12px" }}>
-                    <FontAwesomeIcon icon={faEnvelope} size="lg" className="fontawesome" />
-                    <span className="contacts--texts">info@email.com</span>
+                <div className="contact--wrapper--phones">
+                    <div className="contact--wrapper--phones--iphone" >
+                        <img src="/assets/contact-pic.png" alt="phone-image" />
+                    </div>
+                    <div className="contact--wrapper--phones--circle1" />
+                    <div className="contact--wrapper--phones--circle2" />
                 </div>
-                <div className="contacts--text-wrappers" style={{ paddingBottom: "36px" }}>
-                    <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" className="fontawesome" style={{ paddingRight: "26px" }} />
-                    <span className="contacts--texts">1750 13th St, Boulder, CO 80302</span>
-                </div> */}
             </div>
         </div>
     )
