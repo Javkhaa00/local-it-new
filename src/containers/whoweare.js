0f78b741149b7579e2cd282cb499f5b2b4c712be
Javkhaa00 <liplicker0423@gmail.com>
2020-09-23 20:27:08 +0800

    feat: 1920 done

--- src/containers/whoweare.js
+++ src/containers/whoweare.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Data from '../json/whoweare.json';
 
 const WhoWeAre = () => {
+
+    const returnCard = (el) => {
+
+        if (!el) return null;
+
+        return (
+            <div className="whoweare--wrapper--cards">
+                <img style={{ marginTop: "-40%" }} src={el.imgUrl} alt="profile-pic" />
+                <span className="whoweare--wrapper--cards--title">{el.name}</span>
+                <span className="whoweare--wrapper--cards--position">
+                    {el.position}
+                </span>
+                <span className="whoweare--wrapper--cards--desc">{el.text}</span>
+            </div>
+        );
+    };
+
     return (
         <div className="whoweare">
             <span className="whoweare--title">Who We Are</span>
             <div className="whoweare--wrapper">
-                
+                {Data.map(returnCard)}
             </div>
         </div>
     )
